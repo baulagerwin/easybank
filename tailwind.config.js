acd24c04abbc83037a4849f2421ecb7154590ce9
Gerwin Baula <baulagerwin@gmail.com>
2023-05-18 20:34:35 +0800

    add: dev setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkBlue: "hsl(233, 26%, 24%)",
+        limeGreen: "hsl(136, 65%, 51%)",
+        brightCyan: "hsl(192, 70%, 51%)",
+        grayishBlue: "hsl(233, 8%, 62%)",
+        lightGrayishBlue: "hsl(220, 16%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
